a2d1eb9afc329ef9fa683aaf9f8f9d88eedf232f
LKS9616 <lks609591@gmail.com>
2024-06-18 18:06:45 +0900

    커밋

--- src/main/java/com/ohgiraffers/applications/EditEmp.java
+++ src/main/java/com/ohgiraffers/applications/EditEmp.java
@@ -1,4 +1,20 @@
 package com.ohgiraffers.applications;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+import static com.ohgiraffers.common.JDBCTemplate.getConnection;
+
 public class EditEmp {
-}
+    public static void main(String[] args) {
+
+        Connection con = getConnection();
+
+        PreparedStatement pstmt = null;
+
+        int result = 0;
+
+        Properties prop = new Properties();
+
+    }
